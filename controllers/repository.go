3f26ca7ce67fec7acaf3fccada090d131b2eb718
Meaglith Ma <genedna@gmail.com>
2014-04-14 23:45:07 +0800

    Add repository routes and controller method.

--- controllers/repository.go
+++ controllers/repository.go
@@ -12,3 +12,23 @@ func (this *RepositoryController) Prepare() {
 	this.Ctx.Output.Context.ResponseWriter.Header().Set("X-Docker-Registry-Version", beego.AppConfig.String("Version"))
 	this.Ctx.Output.Context.ResponseWriter.Header().Set("X-Docker-Registry-Standalone", beego.AppConfig.String("Standalone"))
 }
+
+func (this *RepositoryController) PUTRepository() {
+
+}
+
+func (this *RepositoryController) PUTRepositoryImages() {
+
+}
+
+func (this *RepositoryController) GETRepositoryImages() {
+
+}
+
+func (this *RepositoryController) DELETERepositoryImages() {
+
+}
+
+func (this *RepositoryController) PUTRepositoryAuth() {
+
+}
--- routers/router.go
+++ routers/router.go
@@ -14,6 +14,12 @@ func init() {
 	beego.Router("/users", &controllers.UsersController{}, "get:GETUsers")
 	beego.Router("/users", &controllers.UsersController{}, "post:POSTUsers")
 	beego.Router("/users/:username", &controllers.UsersController{}, "put:PUTUsers")
+	//Repository
+	beego.Router("/v1/repositories/:username/:repository", &controllers.RepositoryController{}, "put:PUTRepository")
+	beego.Router("/v1/repositories/:username/:repository/images", &controllers.RepositoryController{}, "put:PUTRepositoryImages")
+	beego.Router("/v1/repositories/:username/:repository/images", &controllers.RepositoryController{}, "get:GETRepositoryImages")
+	beego.Router("/v1/repositories/:username/:repository/images", &controllers.RepositoryController{}, "delete:DELETERepositoryImages")
+	beego.Router("/v1/repositories/:username/:repository/auth", &controllers.RepositoryController{}, "put:PUTRepositoryAuth")
 	//Images
 	beego.Router("/v1/private_images/:image_id/layer", &controllers.ImageController{}, "get:GETPrivateLayer")
 	beego.Router("/v1/images/:image_id/layer", &controllers.ImageController{}, "get:GETLayer")
@@ -25,6 +31,4 @@ func init() {
 	beego.Router("/v1/images/:image_id/json", &controllers.ImageController{}, "put:PUTJSON")
 	beego.Router("/v1/private_images/:image_id/files", &controllers.ImageController{}, "get:GETPrivateFiles")
 	beego.Router("/v1/images/:image_id/files", &controllers.ImageController{}, "get:GETFiles")
-	//Repository
-
 }
