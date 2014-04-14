9db907d8791336019f2329acb87270f27222b6ca
Meaglith Ma <genedna@gmail.com>
2014-04-14 23:16:18 +0800

    Complete the users URL and controller.

--- controllers/users.go
+++ controllers/users.go
@@ -13,6 +13,14 @@ func (this *UsersController) Prepare() {
 	this.Ctx.Output.Context.ResponseWriter.Header().Set("X-Docker-Registry-Standalone", beego.AppConfig.String("Standalone"))
 }
 
-func (this *UsersController) Get() {
+func (this *UsersController) GETUsers() {
 	this.Ctx.Output.Body([]byte("\"OK\""))
+}
+
+func (this *UsersController) POSTUsers() {
+
+}
+
+func (this *UsersController) PUTUsers() {
+
 }
--- routers/router.go
+++ routers/router.go
@@ -1,26 +1,30 @@
 package routers
 
 import (
-  "github.com/astaxie/beego"
-  "github.com/docker-registry/controllers"
+	"github.com/astaxie/beego"
+	"github.com/docker-registry/controllers"
 )
 
 func init() {
-  beego.Router("/", &controllers.MainController{})
-  //Status
-  beego.Router("/_ping", &controllers.PingController{})
-  beego.Router("/v1/_ping", &controllers.PingController{})
-  //Users
-  beego.Router("/users", &controllers.UsersController{})
-  //Images
-  beego.Router("/v1/private_images/:image_id/layer", &controllers.ImageController{}, "get:GETPrivateLayer")
-  beego.Router("/v1/images/:image_id/layer", &controllers.ImageController{}, "get:GETLayer")  
-  beego.Router("/v1/images/:image_id/layer", &controllers.ImageController{}, "put:PUTLayer")
-  beego.Router("/v1/images/:image_id/checksum", &controllers.ImageController{}, "put:PUTChecksum")
-  beego.Router("/v1/private_images/:image_id/json", &controllers.ImageController{}, "get:GETPrivateJSON")
-  beego.Router("/v1/images/:image_id/json", &controllers.ImageController{}, "get:GETJSON")
-  beego.Router("/v1/images/:image_id/ancestry", &controllers.ImageController{}, "get:GETAncestry")
-  beego.Router("/v1/images/:image_id/json", &controllers.ImageController{}, "put:PUTJSON")
-  beego.Router("/v1/private_images/:image_id/files", &controllers.ImageController{}, "get:GETPrivateFiles")
-  beego.Router("/v1/images/:image_id/files", &controllers.ImageController{}, "get:GETFiles")
+	beego.Router("/", &controllers.MainController{})
+	//Status
+	beego.Router("/_ping", &controllers.PingController{})
+	beego.Router("/v1/_ping", &controllers.PingController{})
+	//Users
+	beego.Router("/users", &controllers.UsersController{}, "get:GETUsers")
+	beego.Router("/users", &controllers.UsersController{}, "post:POSTUsers")
+	beego.Router("/users/:username", &controllers.UsersController{}, "put:PUTUsers")
+	//Images
+	beego.Router("/v1/private_images/:image_id/layer", &controllers.ImageController{}, "get:GETPrivateLayer")
+	beego.Router("/v1/images/:image_id/layer", &controllers.ImageController{}, "get:GETLayer")
+	beego.Router("/v1/images/:image_id/layer", &controllers.ImageController{}, "put:PUTLayer")
+	beego.Router("/v1/images/:image_id/checksum", &controllers.ImageController{}, "put:PUTChecksum")
+	beego.Router("/v1/private_images/:image_id/json", &controllers.ImageController{}, "get:GETPrivateJSON")
+	beego.Router("/v1/images/:image_id/json", &controllers.ImageController{}, "get:GETJSON")
+	beego.Router("/v1/images/:image_id/ancestry", &controllers.ImageController{}, "get:GETAncestry")
+	beego.Router("/v1/images/:image_id/json", &controllers.ImageController{}, "put:PUTJSON")
+	beego.Router("/v1/private_images/:image_id/files", &controllers.ImageController{}, "get:GETPrivateFiles")
+	beego.Router("/v1/images/:image_id/files", &controllers.ImageController{}, "get:GETFiles")
+	//Repository
+
 }
