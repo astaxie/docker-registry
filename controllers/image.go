38d588539f8efe1df2168d7b8277d337cae13928
Meaglith Ma <genedna@gmail.com>
2014-04-14 18:13:42 +0800

    Add controller struct at image and repository.

--- controllers/image.go
+++ controllers/image.go
@@ -1,1 +1,16 @@
 package controllers
+
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type ImageController struct {
+	beego.Controller
+}
+
+func (this *ImageController) Prepare() {
+	this.Ctx.Output.Context.ResponseWriter.Header().Set("X-Docker-Registry-Version", beego.AppConfig.String("Version"))
+	this.Ctx.Output.Context.ResponseWriter.Header().Set("X-Docker-Registry-Standalone", beego.AppConfig.String("Standalone"))
+}
--- controllers/repository.go
+++ controllers/repository.go
@@ -1,1 +1,14 @@
 package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type RepositoryController struct {
+	beego.Controller
+}
+
+func (this *RepositoryController) Prepare() {
+	this.Ctx.Output.Context.ResponseWriter.Header().Set("X-Docker-Registry-Version", beego.AppConfig.String("Version"))
+	this.Ctx.Output.Context.ResponseWriter.Header().Set("X-Docker-Registry-Standalone", beego.AppConfig.String("Standalone"))
+}
