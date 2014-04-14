1710ea956c66be2857d8e2a3188ebf31ab492066
Meaglith Ma <genedna@gmail.com>
2014-04-14 17:55:46 +0800

    Add more commet on /v1/_ping.

--- controllers/ping.go
+++ controllers/ping.go
@@ -20,6 +20,11 @@ func (this *PingController) Prepare() {
 // GET /_ping or /v1/_ping
 // API Spec GET /_ping http://docs.docker.io/en/latest/reference/api/registry_api
 // Section 2.4 Status
+// Every 'docker pull' or 'docker push' command will access /v1/_ping before access other URLs.
+// The docker client call at docker/registry/registry.go@pingRegistryEndpoint function.
+// if 'X-Docker-Registry-Version' is blank, the docker client function will return error.
+// if 'X-Docker-Registry-Standalone' is blank, the docker client will assume it's a earlier registry version.
+// So 'X-Docker-Registry-Standalone' must be 'true' or '1'.
 func (this *PingController) Get() {
 	pingResult := PingResult{Result: true}
 	this.Data["json"] = &pingResult
