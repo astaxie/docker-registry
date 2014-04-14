c2e189882da27e71d5ba1f24b658140c636ca89c
Meaglith Ma <genedna@gmail.com>
2014-04-14 19:16:44 +0800

    Add images URLs.

--- controllers/image.go
+++ controllers/image.go
@@ -14,3 +14,43 @@ func (this *ImageController) Prepare() {
 	this.Ctx.Output.Context.ResponseWriter.Header().Set("X-Docker-Registry-Version", beego.AppConfig.String("Version"))
 	this.Ctx.Output.Context.ResponseWriter.Header().Set("X-Docker-Registry-Standalone", beego.AppConfig.String("Standalone"))
 }
+
+func (this *ImageController) GETPrivateLayer() {
+
+}
+
+func (this *ImageController) GETLayer() {
+
+}
+
+func (this *ImageController) PUTLayer() {
+
+}
+
+func (this *ImageController) PUTChecksum() {
+
+}
+
+func (this *ImageController) GETPrivateJSON() {
+
+}
+
+func (this *ImageController) GETJSON() {
+
+}
+
+func (this *ImageController) GETAncestry() {
+
+}
+
+func (this *ImageController) PUTJSON() {
+
+}
+
+func (this *ImageController) GETPrivateFiles() {
+
+}
+
+func (this *ImageController) GETFiles() {
+	
+}
--- routers/router.go
+++ routers/router.go
@@ -12,4 +12,15 @@ func init() {
   beego.Router("/v1/_ping", &controllers.PingController{})
   //Users
   beego.Router("/users", &controllers.UsersController{})
+  //Images
+  beego.Router("/v1/private_images/:image_id/layer", &controllers.ImageController{}, "get:GETPrivateLayer")
+  beego.Router("/v1/images/:image_id/layer", &controllers.ImageController{}, "get:GETLayer")  
+  beego.Router("/v1/images/:image_id/layer", &controllers.ImageController{}, "put:PUTLayer")
+  beego.Router("/v1/images/:image_id/checksum", &controllers.ImageController{}, "put:PUTChecksum")
+  beego.Router("/v1/private_images/:image_id/json", &controllers.ImageController{}, "get:GETPrivateJSON")
+  beego.Router("/v1/images/:image_id/json", &controllers.ImageController{}, "get:GETJSON")
+  beego.Router("/v1/images/:image_id/ancestry", &controllers.ImageController{}, "get:GETAncestry")
+  beego.Router("/v1/images/:image_id/json", &controllers.ImageController{}, "put:PUTJSON")
+  beego.Router("/v1/private_images/:image_id/files", &controllers.ImageController{}, "get:GETPrivateFiles")
+  beego.Router("/v1/images/:image_id/files", &controllers.ImageController{}, "get:GETFiles")
 }
