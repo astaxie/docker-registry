c7144b15eafc84d0d7e2566b5e216f8ab076fefa
Meaglith Ma <genedna@gmail.com>
2014-05-20 09:31:27 -0400

    Cleanup User model

--- controllers/repository.go
+++ controllers/repository.go
@@ -16,156 +16,156 @@ Docker Push & Pull
 package controllers
 
 import (
-	"crypto/md5"
-	"encoding/hex"
-	"fmt"
-	"os"
-	"time"
+  "crypto/md5"
+  "encoding/hex"
+  "fmt"
+  "os"
+  "time"
 
-	"github.com/astaxie/beego"
-	"github.com/dockboard/docker-registry/auth"
-	"github.com/dockboard/docker-registry/models"
-	"github.com/dockboard/docker-registry/utils"
-	"github.com/nu7hatch/gouuid"
+  "github.com/astaxie/beego"
+  "github.com/dockboard/docker-registry/auth"
+  "github.com/dockboard/docker-registry/models"
+  "github.com/dockboard/docker-registry/utils"
+  "github.com/nu7hatch/gouuid"
 )
 
 type RepositoryController struct {
-	beego.Controller
+  beego.Controller
 }
 
 func (this *RepositoryController) Prepare() {
 }
 
 func (this *RepositoryController) PutNamespaceRepo() {
-	fmt.Println("进入PutNamespaceRepo")
-	//判断用户是否合法
-	authorizationBasic := this.Ctx.Input.Header("Authorization")
-	authUsername, authPasswd, authErr := auth.BaseAuth(authorizationBasic)
-	if authErr != nil {
-		this.Ctx.Output.Context.Output.SetStatus(401)
-		this.Ctx.Output.Context.Output.Body([]byte("{\"Unauthorized\"}"))
-		fmt.Println("authErr")
-		return
-	}
+  fmt.Println("进入PutNamespaceRepo")
+  //判断用户是否合法
+  authorizationBasic := this.Ctx.Input.Header("Authorization")
+  authUsername, authPasswd, authErr := auth.BaseAuth(authorizationBasic)
+  if authErr != nil {
+    this.Ctx.Output.Context.Output.SetStatus(401)
+    this.Ctx.Output.Context.Output.Body([]byte("{\"Unauthorized\"}"))
+    fmt.Println("authErr")
+    return
+  }
 
-	//Content-Type: application/json
-	//X-Docker-Registry-Version: 0.6.8
-	//X-Docker-Registry-Config: dev
-	this.Ctx.Output.Context.ResponseWriter.Header().Set("Content-Type", "application/json;charset=UTF-8")
-	this.Ctx.Output.Context.ResponseWriter.Header().Set("X-Docker-Registry-Version", utils.Cfg.MustValue("docker", "XDockerRegistryVersion"))
-	this.Ctx.Output.Context.ResponseWriter.Header().Set("X-Docker-Registry-Config", utils.Cfg.MustValue("docker", "XDockerRegistryConfig"))
+  //Content-Type: application/json
+  //X-Docker-Registry-Version: 0.6.8
+  //X-Docker-Registry-Config: dev
+  this.Ctx.Output.Context.ResponseWriter.Header().Set("Content-Type", "application/json;charset=UTF-8")
+  this.Ctx.Output.Context.ResponseWriter.Header().Set("X-Docker-Registry-Version", utils.Cfg.MustValue("docker", "XDockerRegistryVersion"))
+  this.Ctx.Output.Context.ResponseWriter.Header().Set("X-Docker-Registry-Config", utils.Cfg.MustValue("docker", "XDockerRegistryConfig"))
 
-	dockerRegistryBasePath := utils.Cfg.MustValue("docker", "DockerRegistryBasePath")
-	xDockerEndpoints := utils.Cfg.MustValue("docker", "XDockerEndpoints")
-	strNamespace := string(this.Ctx.Input.Param(":namespace"))
-	strRepoName := string(this.Ctx.Input.Param(":repo_name"))
-	dockerRegistryRepoPath := fmt.Sprintf("%v/repositories/%v/%v", dockerRegistryBasePath, strNamespace, strRepoName)
+  dockerRegistryBasePath := utils.Cfg.MustValue("docker", "DockerRegistryBasePath")
+  xDockerEndpoints := utils.Cfg.MustValue("docker", "XDockerEndpoints")
+  strNamespace := string(this.Ctx.Input.Param(":namespace"))
+  strRepoName := string(this.Ctx.Input.Param(":repo_name"))
+  dockerRegistryRepoPath := fmt.Sprintf("%v/repositories/%v/%v", dockerRegistryBasePath, strNamespace, strRepoName)
 
-	if authUsername != strNamespace {
-		this.Ctx.Output.Context.Output.SetStatus(401)
-		this.Ctx.Output.Context.Output.Body([]byte("{\"username != namespace\"}"))
-		fmt.Println(authUsername, ":authUsername != strNamespace:", strNamespace)
+  if authUsername != strNamespace {
+    this.Ctx.Output.Context.Output.SetStatus(401)
+    this.Ctx.Output.Context.Output.Body([]byte("{\"username != namespace\"}"))
+    fmt.Println(authUsername, ":authUsername != strNamespace:", strNamespace)
 
-		return
-	}
+    return
+  }
 
-	//判断目录是否存在，不存在则创建对应目录
-	if !utils.IsDirExists(dockerRegistryRepoPath) {
-		os.MkdirAll(dockerRegistryRepoPath, os.ModePerm)
-	}
-	//创建token并保存
-	// 需要加密的字符串为 UserName+UserPassword+时间戳
-	md5String := fmt.Sprintf("%v%v%v", authUsername, authPasswd, string(time.Now().Unix()))
-	h := md5.New()
-	h.Write([]byte(md5String))
-	tokenSignature := hex.EncodeToString(h.Sum(nil))
-	xDockerToken := fmt.Sprintf("Token signature=%v,repository=\"%v/%v\",access=write",
-		tokenSignature, strNamespace, strRepoName)
-	mRegistryUser, err := models.GetRegistryUserByUserName(authUsername)
-	if err != nil {
-		this.Ctx.Output.Context.Output.SetStatus(417)
-		this.Ctx.Output.Context.Output.Body([]byte("{\"Expectation failed\"}"))
+  //判断目录是否存在，不存在则创建对应目录
+  if !utils.IsDirExists(dockerRegistryRepoPath) {
+    os.MkdirAll(dockerRegistryRepoPath, os.ModePerm)
+  }
+  //创建token并保存
+  // 需要加密的字符串为 UserName+UserPassword+时间戳
+  md5String := fmt.Sprintf("%v%v%v", authUsername, authPasswd, string(time.Now().Unix()))
+  h := md5.New()
+  h.Write([]byte(md5String))
+  tokenSignature := hex.EncodeToString(h.Sum(nil))
+  xDockerToken := fmt.Sprintf("Token signature=%v,repository=\"%v/%v\",access=write",
+    tokenSignature, strNamespace, strRepoName)
+  mRegistryUser, err := models.GetRegistryUserByUserName(authUsername)
+  if err != nil {
+    this.Ctx.Output.Context.Output.SetStatus(417)
+    this.Ctx.Output.Context.Output.Body([]byte("{\"Expectation failed\"}"))
 
-		return
-	}
-	mRegistryUser.UserToken = xDockerToken
-	models.UpRegistryUser(mRegistryUser)
+    return
+  }
+  mRegistryUser.Token = xDockerToken
+  models.UpRegistryUser(mRegistryUser)
 
-	//X-Docker-Token: Token signature=OT63AV22Y5CGZV7N,repository="dockerfile/redis",access=write
-	//WWW-Authenticate: Token signature=OT63AV22Y5CGZV7N,repository="dockerfile/redis",access=write
-	//X-Docker-Endpoints: 192.168.1.132:5000
-	this.Ctx.Output.Context.ResponseWriter.Header().Set("X-Docker-Token", xDockerToken)
-	this.Ctx.Output.Context.ResponseWriter.Header().Set("WWW-Authenticate", xDockerToken)
-	this.Ctx.Output.Context.ResponseWriter.Header().Set("X-Docker-Endpoints", xDockerEndpoints)
+  //X-Docker-Token: Token signature=OT63AV22Y5CGZV7N,repository="dockerfile/redis",access=write
+  //WWW-Authenticate: Token signature=OT63AV22Y5CGZV7N,repository="dockerfile/redis",access=write
+  //X-Docker-Endpoints: 192.168.1.132:5000
+  this.Ctx.Output.Context.ResponseWriter.Header().Set("X-Docker-Token", xDockerToken)
+  this.Ctx.Output.Context.ResponseWriter.Header().Set("WWW-Authenticate", xDockerToken)
+  this.Ctx.Output.Context.ResponseWriter.Header().Set("X-Docker-Endpoints", xDockerEndpoints)
 
-	this.Ctx.Output.Context.Output.Body([]byte("\"\""))
+  this.Ctx.Output.Context.Output.Body([]byte("\"\""))
 }
 
 func (this *RepositoryController) PutRepo() {
 
-	//判断目录是否存在，不存在则创建对应目录
-	dockerRegistryBasePath := utils.Cfg.MustValue("docker", "DockerRegistryBasePath")
-	dockerRegistryRepoPath := fmt.Sprintf("%v/repositories/library/%v", dockerRegistryBasePath, string(this.Ctx.Input.Param(":repo_name")))
-	if !utils.IsDirExists(dockerRegistryRepoPath) {
-		os.MkdirAll(dockerRegistryRepoPath, os.ModePerm)
-	}
+  //判断目录是否存在，不存在则创建对应目录
+  dockerRegistryBasePath := utils.Cfg.MustValue("docker", "DockerRegistryBasePath")
+  dockerRegistryRepoPath := fmt.Sprintf("%v/repositories/library/%v", dockerRegistryBasePath, string(this.Ctx.Input.Param(":repo_name")))
+  if !utils.IsDirExists(dockerRegistryRepoPath) {
+    os.MkdirAll(dockerRegistryRepoPath, os.ModePerm)
+  }
 
-	//返回结果处理
-	//X-Docker-Token: Token signature=NU66YCHG8FK63I3G,repository="library/redis",access=write
-	tokenSignature, err := uuid.NewV5(uuid.NamespaceURL, []byte(this.Ctx.Input.Url()))
-	if err != nil {
-		fmt.Println("error:", err)
-		return
-	}
-	xDockerToken := fmt.Sprintf("Token signature=%v,repository=\"library/%v\",access=write",
-		tokenSignature, string(this.Ctx.Input.Param(":repo_name")))
-	xDockerEndpoints := utils.Cfg.MustValue("docker", "XDockerEndpoints")
+  //返回结果处理
+  //X-Docker-Token: Token signature=NU66YCHG8FK63I3G,repository="library/redis",access=write
+  tokenSignature, err := uuid.NewV5(uuid.NamespaceURL, []byte(this.Ctx.Input.Url()))
+  if err != nil {
+    fmt.Println("error:", err)
+    return
+  }
+  xDockerToken := fmt.Sprintf("Token signature=%v,repository=\"library/%v\",access=write",
+    tokenSignature, string(this.Ctx.Input.Param(":repo_name")))
+  xDockerEndpoints := utils.Cfg.MustValue("docker", "XDockerEndpoints")
 
-	this.Ctx.Output.ContentType("application/json")
-	this.Ctx.Output.Header("X-Docker-Token", xDockerToken)
-	this.Ctx.Output.Header("WWW-Authenticate", xDockerToken)
-	this.Ctx.Output.Header("X-Docker-Endpoints", xDockerEndpoints)
-	this.Ctx.Output.Header("X-Docker-Registry-Version", utils.Cfg.MustValue("docker", "XDockerRegistryVersion"))
-	this.Ctx.Output.Header("X-Docker-Registry-Config", utils.Cfg.MustValue("docker", "XDockerRegistryConfig"))
+  this.Ctx.Output.ContentType("application/json")
+  this.Ctx.Output.Header("X-Docker-Token", xDockerToken)
+  this.Ctx.Output.Header("WWW-Authenticate", xDockerToken)
+  this.Ctx.Output.Header("X-Docker-Endpoints", xDockerEndpoints)
+  this.Ctx.Output.Header("X-Docker-Registry-Version", utils.Cfg.MustValue("docker", "XDockerRegistryVersion"))
+  this.Ctx.Output.Header("X-Docker-Registry-Config", utils.Cfg.MustValue("docker", "XDockerRegistryConfig"))
 
-	this.Ctx.Output.Body([]byte("\"\""))
+  this.Ctx.Output.Body([]byte("\"\""))
 
 }
 
 func (this *RepositoryController) PutNamespaceTag() {
-	//保存Tag信息
-	nowPutTag := new(models.RegistryRepositorieTag)
+  //保存Tag信息
+  nowPutTag := new(models.RegistryRepositorieTag)
 
-	nowPutTag.RepositorieTag = string(this.Ctx.Input.CopyBody())
-	nowPutTag.RepositorieTagName = this.Ctx.Input.Param(":tag")
-	nowPutTag.RepositorieTagJson = this.Ctx.Input.Header("User-Agent")
-	nowPutTag.RepositorieTagNamespace = this.Ctx.Input.Param(":namespace")
-	nowPutTag.RepositorieTagRepository = this.Ctx.Input.Param(":repository")
-	models.PutOneTag(nowPutTag)
+  nowPutTag.RepositorieTag = string(this.Ctx.Input.CopyBody())
+  nowPutTag.RepositorieTagName = this.Ctx.Input.Param(":tag")
+  nowPutTag.RepositorieTagJson = this.Ctx.Input.Header("User-Agent")
+  nowPutTag.RepositorieTagNamespace = this.Ctx.Input.Param(":namespace")
+  nowPutTag.RepositorieTagRepository = this.Ctx.Input.Param(":repository")
+  models.PutOneTag(nowPutTag)
 }
 
 func (this *RepositoryController) PutTag() {
-	//保存Tag信息
-	nowPutTag := new(models.RegistryRepositorieTag)
+  //保存Tag信息
+  nowPutTag := new(models.RegistryRepositorieTag)
 
-	nowPutTag.RepositorieTag = string(this.Ctx.Input.CopyBody())
-	nowPutTag.RepositorieTagName = this.Ctx.Input.Param(":tag")
-	nowPutTag.RepositorieTagJson = this.Ctx.Input.Header("User-Agent")
-	nowPutTag.RepositorieTagNamespace = "library"
-	nowPutTag.RepositorieTagRepository = this.Ctx.Input.Param(":repository")
-	models.PutOneTag(nowPutTag)
+  nowPutTag.RepositorieTag = string(this.Ctx.Input.CopyBody())
+  nowPutTag.RepositorieTagName = this.Ctx.Input.Param(":tag")
+  nowPutTag.RepositorieTagJson = this.Ctx.Input.Header("User-Agent")
+  nowPutTag.RepositorieTagNamespace = "library"
+  nowPutTag.RepositorieTagRepository = this.Ctx.Input.Param(":repository")
+  models.PutOneTag(nowPutTag)
 
 }
 
 func (this *RepositoryController) PutNamespaceImages() {
-	//这里应该计算checksum
-	//返回204
-	this.Ctx.Output.Context.Output.SetStatus(204)
+  //这里应该计算checksum
+  //返回204
+  this.Ctx.Output.Context.Output.SetStatus(204)
 
 }
 
 func (this *RepositoryController) PutImages() {
-	this.Ctx.Output.Context.Output.SetStatus(204)
+  this.Ctx.Output.Context.Output.SetStatus(204)
 }
 
 func (this *RepositoryController) GETTags() {
--- models/models.go
+++ models/models.go
@@ -1,196 +1,180 @@
 package models
 
 import (
-	"fmt"
-	"log"
+  "fmt"
+  "log"
 
-	"github.com/astaxie/beego"
-	"github.com/dockboard/docker-registry/utils"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/go-xorm/xorm"
+  "github.com/astaxie/beego"
+  "github.com/dockboard/docker-registry/utils"
+  _ "github.com/go-sql-driver/mysql"
+  "github.com/go-xorm/xorm"
 )
 
 var x *xorm.Engine
 
-type RegistryUser struct {
-	Id           int64
-	UserName     string `xorm:"VARCHAR(255)"`
-	UserPassword string `xorm:"VARCHAR(255)"`
-	UserEmail    string `xorm:"VARCHAR(255)"`
-	UserToken    string `xorm:"VARCHAR(255)"`
-	//token MD5(UserName+UserPassword+时间戳)
+type User struct {
+  Id       int64
+  Username string `xorm:"VARCHAR(255)"`
+  Password string `xorm:"VARCHAR(255)"`
+  Email    string `xorm:"VARCHAR(255)"`
+  Token    string `xorm:"VARCHAR(255)"` //MD5(Username+Password+Timestamp)
 }
 
 type RegistryImage struct {
-	Id                     int64
-	ImageId                string `xorm:"VARCHAR(255)"`
-	ImageJson              string `xorm:"TEXT"`
-	ImageParentJson        string `xorm:"TEXT"`
-	ImageUploaded          int64
-	ImageCheckSumed        int64
-	XDockerChecksum        string `xorm:"TEXT"`
-	XDockerChecksumPayload string `xorm:"TEXT"`
+  Id                     int64
+  ImageId                string `xorm:"VARCHAR(255)"`
+  ImageJson              string `xorm:"TEXT"`
+  ImageParentJson        string `xorm:"TEXT"`
+  ImageUploaded          int64
+  ImageCheckSumed        int64
+  XDockerChecksum        string `xorm:"TEXT"`
+  XDockerChecksumPayload string `xorm:"TEXT"`
 }
 
-//type RegistryRepositorie struct {
-//	Id              int64
-//	RepositorieName string `xorm:"VARCHAR(255)"`
-//	RepositorieJson string `xorm:"TEXT"`
-//}
-
 type RegistryRepositorieTag struct {
-	Id                       int64
-	RepositorieTagNamespace  string `xorm:"VARCHAR(255)"`
-	RepositorieTagRepository string `xorm:"VARCHAR(255)"`
-	RepositorieTagName       string `xorm:"VARCHAR(255)"`
-	RepositorieTagJson       string `xorm:"TEXT"`
-	RepositorieTag           string `xorm:"VARCHAR(255)"`
-
-	//RepositorieIsCheck bool `xorm:default false`
-
+  Id                       int64
+  RepositorieTagNamespace  string `xorm:"VARCHAR(255)"`
+  RepositorieTagRepository string `xorm:"VARCHAR(255)"`
+  RepositorieTagName       string `xorm:"VARCHAR(255)"`
+  RepositorieTagJson       string `xorm:"TEXT"`
+  RepositorieTag           string `xorm:"VARCHAR(255)"`
 }
 
 func setEngine() {
-	dbHost := utils.Cfg.MustValue("db", "db_host")
-	dbName := utils.Cfg.MustValue("db", "db_name")
-	dbUser := utils.Cfg.MustValue("db", "db_user")
-	dbPasswd := utils.Cfg.MustValue("db", "db_passwd")
+  dbHost := utils.Cfg.MustValue("db", "db_host")
+  dbName := utils.Cfg.MustValue("db", "db_name")
+  dbUser := utils.Cfg.MustValue("db", "db_user")
+  dbPasswd := utils.Cfg.MustValue("db", "db_passwd")
 
-	var err error
-	//root:root@tcp(192.168.168.208:3306)/docker_registry?charset=utf8  <--这个对了
-	//root:root@192.168.168.208:3306/docker_registry?charset=utf8  <--这个已经不对了
-	connStr := fmt.Sprintf("%v:%v@tcp(%v)/%v?charset=utf8",
-		dbUser, dbPasswd,
-		dbHost, dbName)
-	beego.Trace("Initialized database connStr->", connStr)
+  var err error
+  connStr := fmt.Sprintf("%v:%v@tcp(%v)/%v?charset=utf8", dbUser, dbPasswd, dbHost, dbName)
+  beego.Trace("Initialized database connStr ->", connStr)
 
-	x, err = xorm.NewEngine("mysql", connStr)
-	if err != nil {
-		log.Fatalf("models.init -> fail to conntect database: %v", err)
-	}
+  x, err = xorm.NewEngine("mysql", connStr)
+  if err != nil {
+    log.Fatalf("models.init -> fail to conntect database: %v", err)
+  }
 
-	//if beego.RunMode != "pro" {
-	//}
-	x.ShowDebug = true
-	x.ShowErr = true
-	x.ShowSQL = true
+  x.ShowDebug = true
+  x.ShowErr = true
+  x.ShowSQL = true
 
-	beego.Trace("Initialized database ->", dbName)
+  beego.Trace("Initialized database ->", dbName)
 
 }
 
 // InitDb initializes the database.
 func InitDb() {
-	setEngine()
-	err := x.Sync(new(RegistryUser), new(RegistryImage), new(RegistryRepositorieTag))
-	if err != nil {
-		log.Fatalf("models.init -> fail to sync database: %v", err)
-	}
+  setEngine()
+  err := x.Sync(new(User), new(RegistryImage), new(RegistryRepositorieTag))
+  if err != nil {
+    log.Fatalf("models.init -> fail to sync database: %v", err)
+  }
 }
 
 func GetImageById(imageId string) (returnImage *RegistryImage, err error) {
-	returnImage = new(RegistryImage)
-	rows, err := x.Where("image_id=?", imageId).Rows(returnImage)
-	defer rows.Close()
-	if err != nil {
-		returnImage = nil
-		return
-	}
-	if rows.Next() {
-		rows.Scan(returnImage)
-	} else {
-		returnImage = nil
-	}
+  returnImage = new(RegistryImage)
+  rows, err := x.Where("image_id=?", imageId).Rows(returnImage)
+  defer rows.Close()
+  if err != nil {
+    returnImage = nil
+    return
+  }
+  if rows.Next() {
+    rows.Scan(returnImage)
+  } else {
+    returnImage = nil
+  }
 
-	return
+  return
 }
 
 type AuthError string
 
 func (e AuthError) Error() string {
-	return string(e)
+  return string(e)
 }
 
 type OrmError string
 
 func (e OrmError) Error() string {
-	return string(e)
+  return string(e)
 }
 
-func GetRegistryUserByUserName(mUserName string) (returnRegistryUser *RegistryUser, err error) {
-	returnRegistryUser = new(RegistryUser)
-	rows, err := x.Where("user_name=?", mUserName).Rows(returnRegistryUser)
-	if rows.Next() {
-		rows.Scan(returnRegistryUser)
-		return returnRegistryUser, nil
-	} else {
-		return nil, OrmError("get user by name error")
-	}
-
-}
-
-func GetRegistryUserByToken(mUserName string, mToken string) (returnRegistryUser *RegistryUser, err error) {
-	returnRegistryUser = new(RegistryUser)
-	rows, err := x.Where("user_name=? and user_token=?", mUserName, mToken).Rows(returnRegistryUser)
-	if rows.Next() {
-		rows.Scan(returnRegistryUser)
-		return returnRegistryUser, nil
-	} else {
-		return nil, OrmError("get user by token error")
-	}
+func GetRegistryUserByUserName(mUserName string) (returnRegistryUser *User, err error) {
+  returnRegistryUser = new(User)
+  rows, err := x.Where("username=?", mUserName).Rows(returnRegistryUser)
+  if rows.Next() {
+    rows.Scan(returnRegistryUser)
+    return returnRegistryUser, nil
+  } else {
+    return nil, OrmError("get user by name error")
+  }
 
 }
 
-func UpRegistryUser(upRegistryUser *RegistryUser) (err error) {
-	_, err = x.Id(upRegistryUser.Id).Update(upRegistryUser)
-	if err != nil {
-		return err
-	} else {
-		return nil
-	}
+func GetRegistryUserByToken(mUserName string, mToken string) (returnRegistryUser *User, err error) {
+  returnRegistryUser = new(User)
+  rows, err := x.Where("username=? and token=?", mUserName, mToken).Rows(returnRegistryUser)
+  if rows.Next() {
+    rows.Scan(returnRegistryUser)
+    return returnRegistryUser, nil
+  } else {
+    return nil, OrmError("get user by token error")
+  }
+
+}
+
+func UpRegistryUser(upRegistryUser *User) (err error) {
+  _, err = x.Id(upRegistryUser.Id).Update(upRegistryUser)
+  if err != nil {
+    return err
+  } else {
+    return nil
+  }
 }
 
 func GetRegistryUserAuth(authUsername string, authPassword string) (err error) {
-	mRegistryUser := new(RegistryUser)
-	rows, err := x.Where("user_name=? and user_password=?", authUsername, authPassword).Rows(mRegistryUser)
+  mRegistryUser := new(User)
+  rows, err := x.Where("username=? and password=?", authUsername, authPassword).Rows(mRegistryUser)
 
-	if rows.Next() {
-		return nil
-	} else {
-		return AuthError("Auth Error")
-	}
+  if rows.Next() {
+    return nil
+  } else {
+    return AuthError("Auth Error")
+  }
 }
 
 func InsertOneImage(putRegistryImage *RegistryImage) (affected int64, err error) {
-	affected, err = x.InsertOne(putRegistryImage)
-	return
+  affected, err = x.InsertOne(putRegistryImage)
+  return
 }
 
 func UpOneImage(putRegistryImage *RegistryImage) (affected int64, err error) {
-	affected, err = x.Id(putRegistryImage.Id).Update(putRegistryImage)
-	fmt.Println("putRegistryImage.ImageCheckSumed:", putRegistryImage.ImageCheckSumed, "___affected:", affected, "___err:", err)
-	return
+  affected, err = x.Id(putRegistryImage.Id).Update(putRegistryImage)
+  fmt.Println("putRegistryImage.ImageCheckSumed:", putRegistryImage.ImageCheckSumed, "___affected:", affected, "___err:", err)
+  return
 }
 
 func InsertOneTag(insertRegistryRepositorieTag *RegistryRepositorieTag) (affected int64, err error) {
-	affected, err = x.InsertOne(insertRegistryRepositorieTag)
-	return
+  affected, err = x.InsertOne(insertRegistryRepositorieTag)
+  return
 }
 
 func UpOneTag(upRegistryRepositorieTag *RegistryRepositorieTag) (affected int64, err error) {
-	affected, err = x.Id(upRegistryRepositorieTag.Id).Update(upRegistryRepositorieTag)
-	return
+  affected, err = x.Id(upRegistryRepositorieTag.Id).Update(upRegistryRepositorieTag)
+  return
 }
 
 func PutOneTag(upRegistryRepositorieTag *RegistryRepositorieTag) (affected int64, err error) {
-	rows, err := x.Where("repositorie_tag_name=? and repositorie_tag_namespace=? and repositorie_tag_repository=?",
-		upRegistryRepositorieTag.RepositorieTagName,
-		upRegistryRepositorieTag.RepositorieTagNamespace,
-		upRegistryRepositorieTag.RepositorieTagRepository).Rows(upRegistryRepositorieTag)
-	defer rows.Close()
-	if rows.Next() {
-		x.Id(upRegistryRepositorieTag.Id).Delete(upRegistryRepositorieTag)
-	}
-	affected, err = x.InsertOne(upRegistryRepositorieTag)
-	return
+  rows, err := x.Where("repositorie_tag_name=? and repositorie_tag_namespace=? and repositorie_tag_repository=?",
+    upRegistryRepositorieTag.RepositorieTagName,
+    upRegistryRepositorieTag.RepositorieTagNamespace,
+    upRegistryRepositorieTag.RepositorieTagRepository).Rows(upRegistryRepositorieTag)
+  defer rows.Close()
+  if rows.Next() {
+    x.Id(upRegistryRepositorieTag.Id).Delete(upRegistryRepositorieTag)
+  }
+  affected, err = x.InsertOne(upRegistryRepositorieTag)
+  return
 }
