86a6713a75603dcf07ccf37325ecb998d6af8c7f
Meaglith Ma <genedna@gmail.com>
2014-05-21 22:45:54 +0800

    Reformat the ping controller.

--- routers/router.go
+++ routers/router.go
@@ -1,40 +1,41 @@
 package routers
 
 import (
-  "github.com/astaxie/beego"
-  "github.com/dockboard/docker-registry/controllers"
+	"github.com/astaxie/beego"
+	"github.com/dockboard/docker-registry/controllers"
 )
 
 func init() {
-  beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
 
-  beego.Router("/_ping", &controllers.PingController{}, "get:GetPing")
-  beego.Router("/_ping/", &controllers.PingController{}, "get:GetPing")
-  beego.Router("/v1/_ping", &controllers.PingController{}, "get:GetPing")
-  beego.Router("/v1/_ping/", &controllers.PingController{}, "get:GetPing")
+	//Ping Router
+	beego.Router("/_ping", &controllers.PingController{}, "get:GetPing")
+	beego.Router("/_ping/", &controllers.PingController{}, "get:GetPing")
+	beego.Router("/v1/_ping", &controllers.PingController{}, "get:GetPing")
+	beego.Router("/v1/_ping/", &controllers.PingController{}, "get:GetPing")
 
-  beego.Router("/v1/users/", &controllers.UsersController{}, "get:GetUsers")
-  beego.Router("/v1/users/", &controllers.UsersController{}, "post:PostUsers")
+	beego.Router("/v1/users/", &controllers.UsersController{}, "get:GetUsers")
+	beego.Router("/v1/users/", &controllers.UsersController{}, "post:PostUsers")
 
-  beego.Router("/v1/repositories/:namespace/:repository/tags/:tag", &controllers.RepositoryController{}, "put:PutNamespaceTag")
-  beego.Router("/v1/repositories/:repository/tags/:tag", &controllers.RepositoryController{}, "put:PutTag")
+	beego.Router("/v1/repositories/:namespace/:repository/tags/:tag", &controllers.RepositoryController{}, "put:PutNamespaceTag")
+	beego.Router("/v1/repositories/:repository/tags/:tag", &controllers.RepositoryController{}, "put:PutTag")
 
-  beego.Router("/v1/repositories/:namespace/:repository/images", &controllers.RepositoryController{}, "put:PutNamespaceImages")
-  beego.Router("/v1/repositories/:repository/images", &controllers.RepositoryController{}, "put:PutImages")
+	beego.Router("/v1/repositories/:namespace/:repository/images", &controllers.RepositoryController{}, "put:PutNamespaceImages")
+	beego.Router("/v1/repositories/:repository/images", &controllers.RepositoryController{}, "put:PutImages")
 
-  beego.Router("/v1/repositories/:namespace/:repo_name/", &controllers.RepositoryController{}, "put:PutNamespaceRepo")
-  beego.Router("/v1/repositories/:repo_name/", &controllers.RepositoryController{}, "put:PutRepo")
+	beego.Router("/v1/repositories/:namespace/:repo_name/", &controllers.RepositoryController{}, "put:PutNamespaceRepo")
+	beego.Router("/v1/repositories/:repo_name/", &controllers.RepositoryController{}, "put:PutRepo")
 
-  beego.Router("/v1/repositories/:namespace/:repo_name", &controllers.RepositoryController{}, "put:PutNamespaceRepo")
-  beego.Router("/v1/repositories/:repo_name", &controllers.RepositoryController{}, "put:PutRepo")
+	beego.Router("/v1/repositories/:namespace/:repo_name", &controllers.RepositoryController{}, "put:PutNamespaceRepo")
+	beego.Router("/v1/repositories/:repo_name", &controllers.RepositoryController{}, "put:PutRepo")
 
-  beego.Router("/v1/images/:image_id/json", &controllers.ImageController{}, "get:GetImageJson")
-  beego.Router("/v1/images/:image_id/json", &controllers.ImageController{}, "put:PutImageJson")
+	beego.Router("/v1/images/:image_id/json", &controllers.ImageController{}, "get:GetImageJson")
+	beego.Router("/v1/images/:image_id/json", &controllers.ImageController{}, "put:PutImageJson")
 
-  beego.Router("/v1/images/:image_id/layer", &controllers.ImageController{}, "put:PutImageIdLayer")
+	beego.Router("/v1/images/:image_id/layer", &controllers.ImageController{}, "put:PutImageIdLayer")
 
-  beego.Router("/v1/images/:image_id/checksum", &controllers.ImageController{}, "put:PutChecksum")
+	beego.Router("/v1/images/:image_id/checksum", &controllers.ImageController{}, "put:PutChecksum")
 
-  beego.Router("/_status", &controllers.StatusController{})
-  beego.Router("/v1/_status", &controllers.StatusController{})
+	beego.Router("/_status", &controllers.StatusController{})
+	beego.Router("/v1/_status", &controllers.StatusController{})
 }
