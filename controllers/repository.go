269b0a119614703672f423d94bc0c67b114b058d
Meaglith Ma <genedna@gmail.com>
2014-06-01 19:30:59 +0800

    Add docker pull command explain.

--- controllers/repository.go
+++ controllers/repository.go
@@ -1,5 +1,6 @@
 /*
 Docker Push & Pull
+
 执行 docker push 命令流程：
     1. docker 向 registry 服务器注册 repository： PUT /v1/repositories/<username>/<repository> -> PUTRepository()
     2. 参数是 JSON 格式的 <repository> 所有 image 的 id 列表，按照 image 的构建顺序排列。
@@ -12,6 +13,15 @@ Docker Push & Pull
            3.3.3 写入 <image> 的 checksum 信息：PUT /v1/images/<image_id>/checksum -> image.go#PUTChecksum()
        3.4 上传完此 tag 的所有 image 后，向服务器写入 tag 信息：PUT /v1/repositories/(namespace)/(repository)/tags/(tag) -> PUTTag()
     4. 所有 tags 的 image 上传完成后，向服务器发送所有 images 的校验信息，PUT /v1/repositories/(namespace)/(repo_name)/images -> PUTRepositoryImages()
+
+执行 docker pull 命令流程：
+    1. docker 访问 registry 服务器 repository 的 images 信息：GET /v1/repositories/<username>/<repository>/images -> GetRepositoryImages()
+    2. docker 访问 registry 服务器 repository 的 tags 信息：GET /v1/repositoies/<username>/<repository>/tags -> GetRepositoryTags()
+    3. 根据 <repository> 的 <tags> 中 image 信息进行循环：
+      3.1 获取 <image> 的 Ancestry 信息：GET /v1/images/<image_id>/ancestry -> GetImageAncestry()
+      3.2 获取 <image> 的 JSON 数据: GET /v1/images/<image_id>/json -> GetImageJson()
+      3.3 获取 <image> 的 Layer 文件: GET /v1/images/<image_id/layer -> GetImageLayer()
+
 */
 package controllers
 
