5f85a3ca992f583a7520376139ff0ba12c71cba3
Meaglith Ma <genedna@gmail.com>
2014-06-06 03:32:27 -0400

    Add organization model

--- models/models.go
+++ models/models.go
@@ -1,44 +1,44 @@
 package models
 
 import (
-	"fmt"
-	"github.com/astaxie/beego"
-	"github.com/dockboard/docker-registry/utils"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/go-xorm/xorm"
-	"log"
+  "fmt"
+  "github.com/astaxie/beego"
+  "github.com/dockboard/docker-registry/utils"
+  _ "github.com/go-sql-driver/mysql"
+  "github.com/go-xorm/xorm"
+  "log"
 )
 
 var Engine *xorm.Engine
 
 func setEngine() {
-	host := utils.Cfg.MustValue("mysql", "Host")
-	name := utils.Cfg.MustValue("mysql", "Name")
-	user := utils.Cfg.MustValue("mysql", "User")
-	passwd := utils.Cfg.MustValue("mysql", "Passwd")
+  host := utils.Cfg.MustValue("mysql", "Host")
+  name := utils.Cfg.MustValue("mysql", "Name")
+  user := utils.Cfg.MustValue("mysql", "User")
+  passwd := utils.Cfg.MustValue("mysql", "Passwd")
 
-	var err error
-	conn := fmt.Sprintf("%v:%v@tcp(%v)/%v?charset=utf8", user, passwd, host, name)
-	beego.Trace("Initialized database connStr ->", conn)
+  var err error
+  conn := fmt.Sprintf("%v:%v@tcp(%v)/%v?charset=utf8", user, passwd, host, name)
+  beego.Trace("Initialized database connStr ->", conn)
 
-	Engine, err = xorm.NewEngine("mysql", conn)
-	if err != nil {
-		log.Fatalf("models.init -> fail to conntect database: %v", err)
-	}
+  Engine, err = xorm.NewEngine("mysql", conn)
+  if err != nil {
+    log.Fatalf("models.init -> fail to conntect database: %v", err)
+  }
 
-	Engine.ShowDebug = true
-	Engine.ShowErr = true
-	Engine.ShowSQL = true
+  Engine.ShowDebug = true
+  Engine.ShowErr = true
+  Engine.ShowSQL = true
 
-	beego.Trace("Initialized database ->", name)
+  beego.Trace("Initialized database ->", name)
 
 }
 
 // InitDb initializes the database.
 func InitDb() {
-	setEngine()
-	err := Engine.Sync(new(User), new(Profile), new(Image), new(Repository), new(Tag), new(Comment), new(Star))
-	if err != nil {
-		log.Fatalf("models.init -> fail to sync database: %v", err)
-	}
+  setEngine()
+  err := Engine.Sync(new(User), new(Profile), new(Organization), new(Member), new(Image), new(Repository), new(Tag), new(Comment), new(Star))
+  if err != nil {
+    log.Fatalf("models.init -> fail to sync database: %v", err)
+  }
 }
--- models/user.go
+++ models/user.go
@@ -1,33 +1,55 @@
 package models
 
 import (
-	"time"
+  "time"
 )
 
 type User struct {
-	Id       int64
-	Username string `xorm:"unique not null"`
-	Password string
-	Email    string `xorm:"unique not null"`
-	Token    string
-	Session  string    `xorm:"unique"`
-	Quota    int       `xorm:"default 20"`
-	Size     int       `xorm:"default 2048"`
-	Actived  bool      `xorm:"default false"`
-	Created  time.Time `xorm:"created"`
-	Updated  time.Time `xorm:"updated"`
-	Version  int       `xorm:"version"`
+  Id       int64
+  Username string `xorm:"unique not null"`
+  Password string
+  Email    string `xorm:"unique not null"`
+  Token    string
+  Session  string    `xorm:"unique"`
+  Quota    int64     `xorm:"default 100"`
+  Size     int64     `xorm:"default 2048"`
+  Actived  bool      `xorm:"default false"`
+  Created  time.Time `xorm:"created"`
+  Updated  time.Time `xorm:"updated"`
+  Version  int       `xorm:"version"`
 }
 
 type Profile struct {
-	Id       string
-	UserId   int64 `xorm:"unique not null 'user_id'"`
-	Fullname string
-	Company  string
-	Location string
-	URL      string `xorm:"text 'url'"`
-	Gravatar string
-	Created  time.Time `xorm:"created"`
-	Updated  time.Time `xorm:"updated"`
-	Version  int       `xorm:"version"`
+  Id       string
+  UserId   int64 `xorm:"unique not null 'user_id'"`
+  Fullname string
+  Company  string
+  Location string
+  URL      string `xorm:"text 'url'"`
+  Gravatar string
+  Created  time.Time `xorm:"created"`
+  Updated  time.Time `xorm:"updated"`
+  Version  int       `xorm:"version"`
+}
+
+type Organization struct {
+  Id      int64
+  Name    string `xorm:"unique not null"`
+  Email   string
+  Quota   int64     `xorm:"default 200"`
+  Size    int64     `xorm:"default 4096"`
+  Actived bool      `xorm:"default 0"`
+  Created time.Time `xorm:"created"`
+  Updated time.Time `xorm:"updated"`
+  Version int       `xorm:"version"`
+}
+
+type Member struct {
+  Id             int64
+  OrganizationId int64     `xorm:"not null 'organization_id'"`
+  UserId         int64     `xorm:"not null 'user_id'"`
+  Actived        bool      `xorm:"default 0"`
+  Created        time.Time `xorm:"created"`
+  Updated        time.Time `xorm:"updated"`
+  Version        int       `xorm:"version"`
 }
